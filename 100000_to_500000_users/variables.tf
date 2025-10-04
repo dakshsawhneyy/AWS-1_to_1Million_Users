5ce7846f5957402980e3680dc71274d7e00473db
Daksh Sawhney <darkstardaksh@gmail.com>
2025-10-04 16:47:19 +0530

    Trying to handle 5 lakh users

---
+++ 100000_to_500000_users/variables.tf
@@ -0,0 +1,50 @@
+variable "cluster_name" {
+  description = "This project is for 10000 to 100000 AWS users"
+  default = "10000to100000users"
+  type = string
+}
+variable "project_name" {
+  description = "This project is for 10000 to 100000 AWS users"
+  default = "10000to100000users"
+  type = string
+}
+
+variable "vpc_cidr" {
+  description = "This provides the cidr block for vpc"
+  default = "10.0.0.0/16"
+  type = string
+}
+
+variable "environment" {
+  default = "dev"
+  type = string
+}
+
+variable "single_nat_gateway" {
+  default = true
+  type = bool
+}
+
+variable "kubernetes_version" {
+  description = "Kubernetes version for EKS cluster"
+  type        = string
+  default     = "1.29"
+}
+
+variable "aws_region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "argocd_namespace" {
+  description = "Namespace to install ArgoCD"
+  type        = string
+  default     = "argocd"
+}
+
+variable "argocd_chart_version" {
+  description = "ArgoCD Helm chart version"
+  type        = string
+  default     = "5.51.6"
+}
